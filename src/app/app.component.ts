54a0f52b13a9f6c8ac8af8bca10298807994a7be
mohamed laadjal <laadjalmohamed@gmail.com>
2024-09-03 06:09:56 +0200

    feat: new-recipe form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,31 @@ import { RouterOutlet } from '@angular/router';
 import { Recipe } from './models/recipe.model';
 import { RecipeService } from './recipe.service';
 import { RecipeListComponent } from "./components/recipe-list/recipe-list.component";
-
 import { HeaderComponent } from "./components/header/header.component";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RecipeListComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    RecipeListComponent,
+    HeaderComponent,
+    FormsModule, // Add this line
+    ReactiveFormsModule // Add this line
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Fix typo: styleUrl -> styleUrls
 })
 export class AppComponent {
   title = 'recipe-app-v1';
   recipes: Recipe[] = [];
   constructor(private recipeService: RecipeService) {
-}
+  }
 
-ngOnInit() {
-  this.recipes = this.recipeService.getRecipes();
-  this.recipeService.getRecipes();
-  console.log(this.recipes);
-}
-
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+    this.recipeService.getRecipes();
+    console.log(this.recipes);
+  }
 }
--- src/app/components/recipe-form/recipe-form.component.ts
+++ src/app/components/recipe-form/recipe-form.component.ts
@@ -1,28 +1,22 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RecipeService } from '../../recipe.service';
 import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-form',
-  standalone: true,
   templateUrl: './recipe-form.component.html',
   styleUrls: ['./recipe-form.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule]  // Import ReactiveFormsModule directly
 })
 export class RecipeFormComponent {
   recipeForm: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private recipeService: RecipeService,
-    private router: Router
-  ) {
+  constructor(private fb: FormBuilder) {
     this.recipeForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
-      ingredients: this.fb.array([
-        this.fb.control('', Validators.required)
-      ]),
+      ingredients: this.fb.array([this.fb.control('', Validators.required)]),
       instructions: ['', [Validators.required, Validators.minLength(10)]],
       prepTime: ['', [Validators.required, Validators.min(1)]]
     });
@@ -42,12 +36,8 @@ export class RecipeFormComponent {
 
   onSubmit(): void {
     if (this.recipeForm.valid) {
-      const newRecipe: Recipe = {
-        ...this.recipeForm.value,
-        id: Date.now(),  // Génère un ID unique
-      };
-      this.recipeService.saveRecipe(newRecipe);
-      this.router.navigate(['/']); // Redirige vers la page d'accueil ou une autre page
+      const newRecipe = this.recipeForm.value;
+      console.log('Recipe submitted:', newRecipe);
     }
   }
 }
